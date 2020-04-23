93e6ba9c48b525571f73687d994ab654f3250554
binge_glb <binge_glb@binge.com>
2020-04-23 16:23:30 +0800

    第一次提交

---
+++ src/test/java/com/wygl/sbwygl/SbwyglApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wygl.sbwygl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbwyglApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
